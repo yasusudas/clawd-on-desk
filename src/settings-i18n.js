a6c53e96693027d6fc559d216ca065c5b2399ad7
rullerzhou-afk <rullerzhou@gmail.com>
2026-04-28 07:16:14 +0800

    docs: update contributors and agent guide

--- src/settings-i18n.js
+++ src/settings-i18n.js
@@ -732,7 +732,7 @@
     "hongbigtou", "InTimmyDate", "NeizhiTouhu", "xu3stones-cmd", "androidZzT", "Ye-0413", "WanfengzzZ",
     "TaoXieSZ", "ssly", "stickycandy", "Rladmsrl", "YOIMIYA66", "Kevin7Qi", "sefuzhou770801-hub",
     "Tonic-Jin", "seoki180", "sophie-haynes", "PeterShanxin", "CHIANGANGSTER", "JaeHyeon-KAIST", "hhhzxyhhh",
-    "TVpoet", "zeus6768", "anhtrinh919", "tomaioo", "v-avuso", "livlign",
+    "TVpoet", "zeus6768", "anhtrinh919", "tomaioo", "v-avuso", "livlign", "tongguang2",
   ];
 
   root.ClawdSettingsI18n = {
